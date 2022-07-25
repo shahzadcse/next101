3c388417552d821535d146fb9d9b2bae6f000959
Shahzad Hussain <shahzadcse@gmail.com>
2022-07-25 12:53:24 +0400

    next101

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Home page</h1>
-      <a href="/blog">Blog</a>
+
       <Link href="/blog">
         <a>Blog from Link</a>
       </Link>
